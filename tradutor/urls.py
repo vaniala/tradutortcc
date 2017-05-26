4176cdf10265a164d602dda5a7f6f0649f68e633
Vania Almeida <vania.almeida28@hotmail.com>
2017-05-25 21:48:22 -0300

    teste3

--- tradutor/urls.py
+++ tradutor/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from sinal.views import index
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'sinal.views.index')
+    url(r'^$', index)
 ]
