cb15b3382ef5a36bc3380ab62cfee6aaa0db7c48
Vania Almeida <vania.almeida28@hotmail.com>
2017-05-25 22:08:19 -0300

    teste5

--- sinal/urls.py
+++ sinal/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url
-from sinais.views import index, exibir, RegistrarSinalView
+from sinal.views import index, exibir
 
 urlpatterns = [
     url(r'^$', index, name='index'),
     url(r'^sinais', exibir, name='exibir'),
-    # url(r'^sinais/(?P<sinal_id>\d+)$', 'sinais.views.exibir', name='exibir'),
-    # url(r'^registrar/$', RegistrarSinalView.as_view(), name='registrar'),
+
 
 ]
